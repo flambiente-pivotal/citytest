d1dd1ae3baec3380890428da94801ee68a828460
Filippo Lambiente <flambiente@W882520SYJY.local>
2014-10-31 18:08:13 +0100

    change location to Maranello

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Monza Roadshow!";
+        return "Greetings Maranello Roadshow!";
     }
 }
