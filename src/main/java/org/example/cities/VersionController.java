fb2221ba8b2edeb03b93c511bfba285da3ed3ba4
Filippo Lambiente <flambiente@W882520SYJY.local>
2014-10-31 18:03:17 +0100

    change location to Monza

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Milan Roadshow!";
+        return "Greetings Monza Roadshow!";
     }
 }
