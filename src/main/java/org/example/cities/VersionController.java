745bcccee2f67a588b2364c00e5c62b66b7ca661
Filippo Lambiente <flambiente@W882520SYJY.local>
2014-11-21 14:31:07 +0100

    change location to VMware2

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings VMware Roadshow!";
+        return "Greetings VMware2 Roadshow!";
     }
 }
